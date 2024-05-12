3f92633a71f611428bfe60c0a1274df558505be6
qdat <quocdatstore79@gmail.com>
2024-05-13 01:24:36 +0700

    beverage-store

--- src/app/data-display/data-display.component.ts
+++ src/app/data-display/data-display.component.ts
@@ -12,15 +12,27 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class DataDisplayComponent implements OnInit {
   product = inject(HttpClient)
   data: any[] = []
+  sale: any[] = []
   ngOnInit(): void {
     this.fetchData();
+    this.fetchData1();
+
   }
   fetchData() {
     this.product
-    .get('http://localhost:3000/listproducts')
+      .get('http://localhost:3000/listproducts')
       .subscribe((data: any) => {
-        this.data = data;
-        console.log(data);
+        this.data = data.splice(0, 8);
+        // console.log(data);
       })
   }
+  fetchData1() {
+    this.product
+      .get('http://localhost:3000/listproducts')
+      .subscribe((sale: any) => {
+        const filterSale = sale.filter((item: any) => item.sale && item.sale.price);
+        this.sale = filterSale.splice(0, 8);
+      });
+  }
+
 }
