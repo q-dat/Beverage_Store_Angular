044fd408583f1435c8c35030f5cbf565f043c9cc
qdat <quocdatstore79@gmail.com>
2024-05-13 12:56:09 +0700

    beverage-store

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -3,9 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { DetailComponent } from './pages/detail/detail.component';
+
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'shop', component: ShopComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'detail', component: DetailComponent },
+
 ];
