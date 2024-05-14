a26c283207ecd49223ecf720de812d7bfb249c99
qdat <quocdatstore79@gmail.com>
2024-05-14 14:56:14 +0700

    beverage-store

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { DataDisplayComponent } from '../../data-display/data-display.component';
+import { Component, inject } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DataDisplayComponent],
+  imports: [CommonModule, HttpClientModule,],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  product = inject(HttpClient)
+  data: any[] = []
+  sale: any[] = []
+  ngOnInit(): void {
+    this.fetchData();
+    this.fetchData1();
 
+  }
+  fetchData() {
+    this.product
+      .get('http://localhost:3000/listproducts')
+      .subscribe((data: any) => {
+        this.data = data.splice(0, 8);
+        // console.log(data);
+      })
+  }
+  fetchData1() {
+    this.product
+      .get('http://localhost:3000/listproducts')
+      .subscribe((sale: any) => {
+        const filterSale = sale.filter((item: any) => item.sale && item.sale.price);
+        this.sale = filterSale.splice(0, 8);
+      });
+  }
 }
