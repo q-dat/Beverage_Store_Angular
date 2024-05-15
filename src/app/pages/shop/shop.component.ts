11c1220d08fab73c04b1115637a1e3ee121678ba
qdat <quocdatstore79@gmail.com>
2024-05-15 16:15:36 +0700

    beverage-store

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
+  product = inject(HttpClient)
+  data: any[] = []
+  ngOnInit(): void {
+    this.fetchData();
 
+  }
+  fetchData() {
+    this.product
+      .get('http://localhost:3000/listproducts')
+      .subscribe((data: any) => {
+        this.data = data.splice(0, 8);
+        // console.log(data);
+      })
+  }
 }
--- zzzzzzzzzzzzzzzzzzzzzzz.md
+++ zzzzzzzzzzzzzzzzzzzzzzz.md
@@ -1,3 +1,3 @@
 git add .
 git commit -m "beverage-store" 
-git push origin main
+git push origin main 
