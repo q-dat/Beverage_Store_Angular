e2335f861cefed0f14bec65752d18ccb6ef7247a
qdat <quocdatstore79@gmail.com>
2024-05-17 02:36:19 +0700

    beverage-store

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink,CommonModule,RouterModule],
   templateUrl: './App.html',
 })
 
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/Config';
 import { AppComponent } from './app/App';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
