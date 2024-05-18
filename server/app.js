c61ba2e6b98b233dc94e13f2390896aad6d9d13b
qdat <quocdatstore79@gmail.com>
2024-05-19 01:45:50 +0700

    beverage-store

---
+++ server/app.js
@@ -0,0 +1,34 @@
+// Cài đặt các module cần thiết
+const express = require('express');
+const mysql = require('mysql');
+
+// Tạo một ứng dụng Express
+const app = express();
+
+// Cấu hình kết nối cơ sở dữ liệu
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'beverage-store-angular'
+});
+
+// Kết nối với cơ sở dữ liệu
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('--------------------------------------------------------------------Kết Nối CSDL Thành Công!');
+});
+
+// Định nghĩa một API endpoint để lấy dữ liệu từ cơ sở dữ liệu
+app.get('/', (req, res) => {
+  connection.query('SELECT * FROM products', (error, results, fields) => {
+    if (error) throw error;
+    res.json(results);
+  });
+});
+
+// Khởi động server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`--------------------------------------------------------------------http://localhost:${port}`);
+});
---
+++ server/ex.js
@@ -0,0 +1,44 @@
+// Sử dụng CreatPool để kết nối CSDL
+const mysql = require('mysql2/promise');
+const fs = require('fs').promises;
+async function connectdb() {
+    try {
+        const pool = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'beverage-store-angular',
+            waitForConnections: true,
+            connectionLimit: 10,
+            queueLimit: 0
+        });
+        return pool;
+    }
+    catch (error) {
+        console.log('--------------------------------------------------------------------Lỗi kết nối: ' + error);
+        throw error;
+    }
+}
+
+// Get dữ liệu với nhiều option, tạo biến kiểu Object để lưu các kết quả
+async function CreateHomeJsonFile() {
+    try {
+        const pool = await connectdb();
+        // const connection = await pool.getConnection();
+        const [newRows, Rows] = await pool.query('SELECT * FROM products')
+        // const [newCategory, Category] = await pool.query('SELECT * FROM category')
+        const homeData = {
+            Products: newRows,
+            // Category:newCategory,
+        };
+        // Thực hiện ghi dữ liệu thành file json
+        const jsonData = JSON.stringify(homeData, null, 2);
+        await fs.writeFile('db.json', jsonData);
+        console.log('--------------------------------------------------------------------Xuất file JSON thành công');
+        pool.end(); //Kết thúc pool kết nối
+    } catch (error) {
+        console.error('--------------------------------------------------------------------Lỗi xuất file: ', error);
+        throw error;
+    }
+}
+CreateHomeJsonFile();
