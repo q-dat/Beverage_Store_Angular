60f7bb648c74af9c558d36c85cdf6b7e36a66ae0
Test <test@Tests-MacBook-Pro.local>
2024-05-30 00:40:40 +0700

    beverage-store

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink,CommonModule,RouterModule],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,CommonModule,RouterModule],
   templateUrl: './App.html',
 })
 
